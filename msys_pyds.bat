03f2d8a944498ece24b8bbdf4c2a2bfae460034c
Lorenzo Mancini <lmancini@develer.com>
2010-12-18 21:14:04 +0000

    Placeholders for custom user directories, to remove all hardcoded paths within the build process.

--- msys_pyds.bat
+++ msys_pyds.bat
@@ -1,27 +1,28 @@
-SET PATH=/C/MinGW/bin;/c/devkitPro/msys/bin;/c/devkitPro/devkitARM/bin;/c/Python26;/c/Python26/Scripts
-
-SET INCLUDE=/c/devkitPro/devkitARM/arm-eabi/include
-SET LIB=/c/devkitPro/devkitARM/arm-eabi/lib
-
+SET PYTHONDIR=/c/Python26/
 SET DEVKITPRO=/c/devkitPro/
-SET DEVKITARM=/c/devkitPro/devkitARM/
+SET DSPYTHON=/c/devkitPro/dspython/
+SET MINGWDIR=/c/MinGW/
 
+rem
+rem You should not customize anything beyond this point
+rem
+
+SET DEVKITARM=%DEVKITPRO%/devkitARM/
+SET PATH=%MINGWDIR%/bin;%DEVKITPRO%/msys/bin;%DEVKITARM%/bin;%PYTHONDIR%/;%PYTHONDIR%/Scripts
+SET INCLUDE=%DEVKITARM%/arm-eabi/include
+SET LIB=%DEVKITARM%/arm-eabi/lib
 
 rem ndslib
-
-SET NDSLIB_INCLUDE=/C/devkitPro/dspython/libnds-1.4.6/include
-SET NDSLIB_LIB=/C/devkitPro/dspython/libnds-1.4.6/lib
-
+SET NDSLIB_INCLUDE=%DSPYTHON%/libnds-1.4.6/include
+SET NDSLIB_LIB=%DSPYTHON%/libnds-1.4.6/lib
 
 rem stackless
 set ARCH=-mthumb -mthumb-interwork
-set BASECFLAGS=-ffast-math -mthumb -mthumb-interwork -DARM9 -DNDS
-set CFLAGS=-ffast-math -mthumb -mthumb-interwork -DARM9 -DNDS
-set LDFLAGS=-specs=ds_arm9.specs -g -mthumb -mthumb-interwork -lnds9 -L/C/devkitPro/dspython/libnds-1.4.6/lib
+set BASECFLAGS=-ffast-math %ARCH% -DARM9 -DNDS
+set CFLAGS=-ffast-math %ARCH% -DARM9 -DNDS
+set LDFLAGS=-specs=ds_arm9.specs -g %ARCH% -lnds9 -L%NDSLIB_LIB%
 
 set CC_FOR_BUILD=gcc
-
-set PYTHON_FOR_BUILD=/c/Python26/python
-
+set PYTHON_FOR_BUILD=%PYTHONDIR%/python
 
 c:\devkitPro\msys\msys.bat
