0bb7fcf444beae7e26cab1903fd6591f7eddb3e1
Lorenzo Mancini <lmancini@develer.com>
2010-12-19 13:59:55 +0000

    Add a wrap_libnds subproject, whose purpose is contain all pyrex wrappers and produce "spiked" libnds7.a and libnds9.a which also expose the Python functions.

---
+++ wrap_libnds/include/nds/arm9/wrap_rumble.pyx
@@ -0,0 +1,17 @@
+cdef extern from "nds/ndstypes.h":
+    ctypedef int bool
+
+cdef extern from "nds/arm9/rumble.h":
+    bool c_isRumbleInserted "isRumbleInserted" ()
+    void c_setRumble "setRumble" (bool position)
+
+def isRumbleInserted():
+    return c_isRumbleInserted()
+
+def setRumble(position):
+    c_setRumble(position)
+
+def vibrate(int length):
+    for i in range(0, length):
+        setRumble(True)
+        setRumble(False)
